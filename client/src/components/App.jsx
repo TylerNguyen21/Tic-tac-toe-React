b9d684eb307ff7f9a7cfecedffcdcbe95e76c8ea
Tyler Nguyen <tydnguyen@ucdavis.edu>
2019-02-06 14:30:52 -0800

    change value of divs

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -6,16 +6,16 @@ class App extends React.Component {
     super(props)
     this.state = {
       board: [
-        [0,0,0],
-        [0,0,0],
-        [0,0,0]
+        ['','',''],
+        ['','',''],
+        ['','','']
       ],
       playersTurn: 1
     }
     this.handlePlay = this.handlePlay.bind(this);
   }
 
-  handlePlayer(row,col) {
+  handlePlay(row,col) {
     let mark = this.state.playersTurn === 1 ? 'X' : 'O';
     let newBoard = this.state.board
     newBoard[row][col] = mark;
@@ -28,19 +28,19 @@ class App extends React.Component {
     return (
       <div className="board">
         <div className="row1">
-          <div className="cell" onClick= {() => this.handlePlay(0, 0)}></div>
-          <div className="cell" onClick= {() => this.handlePlay(0, 1)}></div>
-          <div className="cell" onClick= {() => this.handlePlay(0, 2)}></div>
+          <div className="cell" onClick= {() => this.handlePlay(0, 0)}>{this.state.board[0][0]}</div>
+          <div className="cell" onClick= {() => this.handlePlay(0, 1)}>{this.state.board[0][1]}</div>
+          <div className="cell" onClick= {() => this.handlePlay(0, 2)}>{this.state.board[0][2]}</div>
         </div>
         <div className="row2">
-          <div className="cell" onClick= {() => this.handlePlay(1, 0)}></div>
-          <div className="cell" onClick= {() => this.handlePlay(1, 1)}></div>
-          <div className="cell" onClick= {() => this.handlePlay(1, 2)}></div>
+          <div className="cell" onClick= {() => this.handlePlay(1, 0)}>{this.state.board[1][0]}</div>
+          <div className="cell" onClick= {() => this.handlePlay(1, 1)}>{this.state.board[1][1]}</div>
+          <div className="cell" onClick= {() => this.handlePlay(1, 2)}>{this.state.board[1][2]}</div>
         </div>
         <div className="row3">
-          <div className="cell" onClick= {() => this.handlePlay(2, 0)}></div>
-          <div className="cell" onClick= {() => this.handlePlay(2, 1)}></div>
-          <div className="cell" onClick= {() => this.handlePlay(2, 2)}></div>
+          <div className="cell" onClick= {() => this.handlePlay(2, 0)}>{this.state.board[2][0]}</div>
+          <div className="cell" onClick= {() => this.handlePlay(2, 1)}>{this.state.board[2][1]}</div>
+          <div className="cell" onClick= {() => this.handlePlay(2, 2)}>{this.state.board[2][2]}</div>
         </div>
       </div>
     )
